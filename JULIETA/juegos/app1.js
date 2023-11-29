1ba1f412a9b5bcca79ad12ef8a0edfe4f9e35cc7
poplils <152444476+poplils@users.noreply.github.com>
2023-11-29 18:19:09 -0300

    version 1 de codigo

---
+++ JULIETA/juegos/app1.js
@@ -0,0 +1,12 @@
+const letterContainer= document.getElementById("letter-container");
+const optionsContainer= document.getElementById("options-container")
+const userInputSection= document.getElementById("user-input-section");
+const newGameContainer= document.getElementById("new-game-container");
+const newGamebutton= document.getElementById("new-game-button");
+const canvas= document.getElementById("canvas");
+const resultText =document.getElementById("result-text");
+
+//camelCase: la primera palabra rpda en minuscula y apartir de la segunda
+//palabra en adelante se pone la primera en mayuscula
+
+let
